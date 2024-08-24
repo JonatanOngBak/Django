adffabff33dfdd21927964b68f8a037897c8e31b
Jonatan <jonathanfds2007@gmail.com>
2024-08-24 11:30:07 -0300

    feat(models) adicionado descrição para class Topic

--- Aula 1/learning_log/learning_logs/models.py
+++ Aula 1/learning_log/learning_logs/models.py
@@ -8,5 +8,16 @@ class Topic(models.Model):
     def __str__(self):
         """Devolve uma representação em uma string modelo."""
         return self.text
-        
+    
 
+class Entry(models.Model):
+    """Algo especifico aprendido sobre um assunto."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)  # o argumento cascade fará com que ele seja deletado tbm caso o tópico relacionado seja deletado
+    text = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + '...'       
