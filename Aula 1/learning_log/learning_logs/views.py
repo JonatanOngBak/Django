b6d08d795c14fc41b9b1ab1ab44e39faf09ba8e1
Jonatan Fernandes <jonathanfds2007@gmail.com>
2024-08-30 13:14:07 -0300

    feat(views) adicionado função topic mostra um unico assunto

--- Aula 1/learning_log/learning_logs/views.py
+++ Aula 1/learning_log/learning_logs/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from .models import Topic
 
-
 def index(request):
-    """pagina principla do learning_log"""
+    """A página inicial de Learning Log"""
     return render(request, 'learning_logs/index.html')
 
 def topics(request):
     """Mostra todos os assuntos"""
-    topic = Topic.objects.order_by('date_added')
+    topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
