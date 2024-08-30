04eb2ddf6146ce949067c284b4298cf4a5808a36
Jonatan <jonathanfds2007@gmail.com>
2024-08-29 21:50:09 -0300

    style(topics) adicionado pagina topicos

--- Aula 1/learning_log/learning_logs/views.py
+++ Aula 1/learning_log/learning_logs/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 def index(request):
     """pagina principla do learning_log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topic = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
